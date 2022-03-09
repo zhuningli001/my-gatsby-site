06f71ff5f0622b36cd7d5459cf7f0bd2ce703e29
zhuningli <zhuningli001@163.com>
2022-03-09 22:50:04 +0100

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 // styles
 const pageStyles = {
-  color: "#232129",
+  color:"red",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
@@ -17,6 +17,8 @@ const headingAccentStyles = {
 const paragraphStyles = {
   marginBottom: 48,
 }
+
+// "src/pages/index.js "
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
@@ -35,10 +37,11 @@ const listItemStyles = {
   marginBottom: 30,
 }
 
+// subtitles/cateogries
 const linkStyle = {
-  color: "#8954A8",
+  color: "red",
   fontWeight: "bold",
-  fontSize: 16,
+  fontSize: 26,
   verticalAlign: "5%",
 }
 
@@ -48,8 +51,9 @@ const docLinkStyle = {
   marginBottom: 24,
 }
 
+//text from: subtitles/cateogries
 const descriptionStyle = {
-  color: "#232129",
+  color: "blue",
   fontSize: 14,
   marginTop: 10,
   marginBottom: 0,
@@ -77,6 +81,8 @@ const badgeStyle = {
   marginLeft: 10,
   lineHeight: 1,
 }
+
+
 
 // data
 const links = [
@@ -129,15 +135,21 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
+      <title> Home Page </title>
+
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations,zhuningli
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        <span style={headingAccentStyles}>— you have just made a Gatsby site! </span>
+        <br />
+        <span style={headingAccentStyles}> 你已经复制了第一个001自己的博客</span>
         <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
+          🎉🎉🎉🔥🔥🔥
+          🎉🎉🎉🔥🔥🔥
         </span>
       </h1>
+
+
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time.{" "}
@@ -145,6 +157,8 @@ const IndexPage = () => {
           😎
         </span>
       </p>
+
+
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
@@ -154,6 +168,7 @@ const IndexPage = () => {
             {docLink.text}
           </a>
         </li>
+        
         {links.map(link => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
@@ -173,7 +188,9 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
-      <img
+
+
+      <img 
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
